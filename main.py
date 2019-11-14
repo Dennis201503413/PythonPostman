c1c05ca2a54358a701a14fa395abf056c505386d
Dennis <dennismasaya@gmail.com>
2019-11-14 09:41:29 -0600

    Threading added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import requests
+import threading
 from datetime import datetime
 from tkinter import *
 from tkinter import filedialog
@@ -21,7 +22,7 @@ Label(m, text='RESUMEN', width=14, anchor='e').grid(row=6,column=0)
 # =========================== INPUTS ===========================
 
 dir = Entry(m,width=63)
-dir.insert(tk.END,'http://localhost:3000/api/tweet')
+dir.insert(tk.END,'http://35.209.204.71:8080/api/tweet')
 dir.grid(row=0,column=1, columnspan=2)
 concurrencia = Spinbox(m,from_=1,to=2147483647, width=61)
 concurrencia.grid(row=1,column=1, columnspan=2)
@@ -56,22 +57,35 @@ def make_get_request():
     current_time = now.strftime('%H:%M:%S')
     resumen.insert(tk.END,current_time)
     resumen.insert(tk.END,'\n')
+    for l in range(int(concurrencia.get())):
+        x = threading.Thread(target=threaded_request)
+        x.start()
+    resumen.insert(tk.END,'\n')
+    resumen.insert(tk.END,'\n')
+    resumen.insert(tk.END,'Tiempo de finalizado:')
+    now2 = datetime.now()
+    current_time2 = now2.strftime('%H:%M:%S')
+    resumen.insert(tk.END,current_time2)
+    resumen.insert(tk.END,'\n')
 
+def threaded_request():
     file = open(archivo.get(), 'r')
     list = file.readlines()
-    for l in list:
-        splitted_list = l.split(';')
-        usr = (splitted_list[0].split('&')[0]).split('=')[1]
-        nom = (splitted_list[1].split('&')[0]).split('=')[1]
-        txt = splitted_list[2].split('=')[1]
-        txt_without_hashtag = (txt.replace("#", "")).replace('\n','')
-        hashtag = txt.split('#', maxsplit=1)[-1].split(maxsplit=1)[0]
-        PARAMS = {'usr':usr,'nom':nom,'txt':txt,'tag':hashtag}
-        URL = dir.get()
-        r = requests.get(url = URL,params=PARAMS)
-        resumen.insert(tk.END,r.json())
+    requests_count = int(solicitudes.get())
+    while requests_count >0:
+        for l in list:
+            splitted_list = l.split(';')
+            usr = (splitted_list[0].split('&')[0]).split('=')[1]
+            nom = (splitted_list[1].split('&')[0]).split('=')[1]
+            txt = splitted_list[2].split('=')[1]
+            txt_without_hashtag = (txt.replace("#", "")).replace('\n','')
+            hashtag = txt.split('#', maxsplit=1)[-1].split(maxsplit=1)[0]
+            PARAMS = {'usr':usr,'nom':nom,'txt':txt,'tag':hashtag}
+            URL = dir.get()
+            r = requests.get(url = URL,params=PARAMS)
+            resumen.insert(tk.END,r.json())
+            requests_count = requests_count-1
         #resumen.insert(tk.END,'usr='+usr+'\nnom='+nom+'\ntxt='+txt_without_hashtag+'\ntag='+hashtag+'\n\n')
-    resumen.insert(tk.END,'Tiempo de finalizado:')
 
 exec_button = Button(m,text='Ejecutar', width=9, command=make_get_request)
 exec_button.grid(row=5,column=2)
